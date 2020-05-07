0db6185edd610b5badc78f9485b303d63ccf3c55
Tyler Bates <Fondlytyler@gmail.com>
2020-05-07 14:36:50 -0700

    successfully integrated all aspects of gameplay and bugfixes.

--- YouGrowGirl.cs
+++ YouGrowGirl.cs
@@ -26,8 +26,8 @@ namespace Plants.Game
       GameActions = new Dictionary<int, Action>() { {1, () => WaterPlant()}, {2, () => SunPlant() }, {3, () => FertilizePlant()}, {4, () => SingToPlant()}, {5, () => WalkPlant()} };
     }
 
-    public static Random rand = new Random();
-    int count = rand.Next(1,3);
+    // public static Random rand = new Random();
+    // int count = rand.Next(1,3);
 
     public void InitiateGame(string name, string species)
     {
@@ -38,9 +38,8 @@ namespace Plants.Game
     public void DetermineNextStep()
     {
       IsGameOver();
-      // Console.WriteLine("Current Water: " + WaterStatus + ", Current Sunshine: " + SunshineStatus + ", Current Fertilizer: " + FertilizerStatus);
       Console.WriteLine("Choose a plant based activity:");
-      Console.WriteLine("1: Water, 2: Sun, 3: Fertilize, 4: Sing (Risky!), 5: Walk (Risky!)");
+      Console.WriteLine("1: Water, 2: Sun, 3: Fertilize, 4: Sing (Risky!), 5: Walk (Reset your bearings!)");
       Console.WriteLine("Enter a number 1-5.");
       int action = int.Parse(Console.ReadLine());
       GameActions[action].Invoke();
@@ -66,14 +65,30 @@ namespace Plants.Game
     {
       FertilizerStatus += 3;
       SunshineStatus -= 1;
-      // DisplayFertilizerStatus();
+      DisplayFertilizerStatus();
       DetermineNextStep();
     }
 
     private void SingToPlant()
     {
-      SingStatus += 1;
-      // DisplaySingStatus();
+      Random rand = new Random();
+      int count = rand.Next(1,3);
+
+      if(count == 1)
+      {
+        WaterStatus = 2;
+        SunshineStatus = 2;
+        FertilizerStatus = 2;
+        SingStatus += 1;
+      }
+      else
+      {
+        WaterStatus -= 1;
+        SunshineStatus -= 1;
+        FertilizerStatus -= 1;
+      }
+      
+      DisplaySingStatus();
       DetermineNextStep();
     }
 
@@ -96,7 +111,7 @@ namespace Plants.Game
       }
       else
       {
-        Console.WriteLine(Name + " is drowning! |X_X| ");  
+        Console.WriteLine(Name + " is drowning! |X_X| help dry them out.");  
       }
     }
 
@@ -105,7 +120,7 @@ namespace Plants.Game
     {
       if(SunshineStatus > 2)
       {
-        Console.WriteLine("It is hot in Topeka! Cool " + Name + " down a bit!");
+        Console.WriteLine("It is hot in Topeka! With a little help, " + Name + " will be ready to grow and ready to go! |> w <|");
       }
       else if(SunshineStatus == 2)
       {
@@ -116,6 +131,34 @@ namespace Plants.Game
         Console.WriteLine("Eternal darkeness. " + Name + "'s life is a static void |X_x|");
       }
     }
+
+    public void DisplayFertilizerStatus()
+    {
+      if(FertilizerStatus < 2)
+      {
+        Console.WriteLine(Name + "'s starving! Their fangs are out RUN FOR YOUR LIFE |>'M'|>.");
+      }
+      else if(FertilizerStatus == 2)
+      {
+        Console.WriteLine("*om nom nom*" + Name + " is perfectly full |^ w ^|");
+      }
+      else{
+        Console.WriteLine(Name + " is too full! <(   *_*   )> Give her something to break down the food!");
+      }
+    }
+
+    public void DisplaySingStatus()
+    {
+      if (SingStatus > 0)
+      {
+        Console.WriteLine("Your beautiful voice has spoken to " + Name + "! Something magical is happening! |0 w 0|");
+      }
+      else
+      {
+        Console.WriteLine("Oof I don't think " + Name + " liked that. They look upsetti sphaghetti |# > n <|");
+      }
+    }
+      
     private void IsGameOver()
     {
       if(WaterStatus == 2 && SunshineStatus == 2 && FertilizerStatus == 2 && Species == "cactus")
