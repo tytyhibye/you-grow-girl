43f38cb22d8c5f85669b1a2009b07ed5f5280195
Tyler Bates <Fondlytyler@gmail.com>
2020-05-07 11:02:14 -0700

    add game initialization functionality with no bugs.

---
+++ Program.cs
@@ -0,0 +1,45 @@
+using System;
+using Plants.Game;
+
+namespace Plants
+{
+  public class Program
+  {
+    public static string DecideSpecies()
+    {
+      Console.WriteLine("Would you like to grow a flower or a cactus? F/C");
+      string speciesInput = (Console.ReadLine().ToLower());
+      if (speciesInput == "f")
+      {
+        return "flower";
+      }
+      else
+      // } else if (speciesInput == "c")
+      {
+        return "cactus";
+      }
+    }
+    public static void Main()
+    {
+      
+      Console.WriteLine(@"
+        __   __            _____                     _____ _      _ 
+        \ \ / /           |  __ \                   |  __ (_)    | |
+         \ V /___  _   _  | |  \/_ __ _____      __ | |  \/_ _ __| |
+          \ // _ \| | | | | | __| '__/ _ \ \ /\ / / | | __| | '__| |
+          | | (_) | |_| | | |_\ \ | | (_) \ V  V /  | |_\ \ | |  | |
+          \_/\___/ \__,_|  \____/_|  \___/ \_/\_/    \____/_|_|  |_| ");
+      // DISPLAY PLANT IMAGE, WELCOME PAGE FONT TOO for ^^^^?
+      Console.WriteLine("What would you like to name your plant?");
+      string name = Console.ReadLine();
+      Console.WriteLine("Welcome to the world " + name + "!");
+
+      string species = DecideSpecies();
+
+      Plant userPlant = new Plant(name, species);
+      userPlant.InitiateGame(name, species);
+
+      
+    }
+  }
+}
--- YouGrowGirl.cs
+++ YouGrowGirl.cs
@@ -1,7 +1,7 @@
 using System;
 using System.Collections.Generic;
 
-namespace Plant.Game
+namespace Plants.Game
 {
   public class Plant{
     public string Name { get; set; }
@@ -14,22 +14,92 @@ namespace Plant.Game
     public int TotalCareStatus { get; set; }
 
 
-  public Plant(string name, string species)
-  {
-    Name = name;
-    Species = species;
-    WaterStatus = 0;
-    SunshineStatus = 0;
-    FertilizerStatus = 0;
-    SingStatus = 0;
-    WalkStatus = 0;
-    TotalCareStatus = 0;
+    public Plant(string name, string species)
+    {
+      Name = name;
+      Species = species;
+      WaterStatus = 0;
+      SunshineStatus = 0;
+      FertilizerStatus = 0;
+      SingStatus = 0;
+      WalkStatus = 0;
+      TotalCareStatus = 0;
+    }
+
+    
+    // private static Dictionary<int, func<string, int>> _gameActions = new Dictionary<int, func<string, int>>() { {1, WaterPlant()}, {2, SunPlant() }, };
+
+    public static Random rand = new Random();
+    int count = rand.Next(1,3);
+
+    public void InitiateGame(string name, string species)
+    {
+      Console.WriteLine(name + " the " + species + " is good to grow!");
+      Console.WriteLine("Choose a plant based activity:");
+      
+
+    }
+
+    public void DetermineNextStep()
+    {
+      WaterPlant();
+      SunPlant();
+      FertilizePlant();
+      SingToPlant();
+      WalkPlant();
+      IsGameOver();
+    }
+
+    private void WaterPlant()
+    {
+      WaterStatus += 1;
+      DetermineNextStep();
+    }
+
+    private void SunPlant()
+    {
+      SunshineStatus += 1;
+      WaterStatus -= 1;
+      DetermineNextStep();
+    }
+
+    private void FertilizePlant()
+    {
+      FertilizerStatus += 1;
+      DetermineNextStep();
+    }
+
+    private void SingToPlant()
+    {
+      SingStatus += 1;
+      DetermineNextStep();
+    }
+
+    private void WalkPlant()
+    {
+      WalkStatus += 1;
+      DetermineNextStep();
+    }
+
+    private void IsGameOver()
+    {
+      if(TotalCareStatus >= 5 && Species == "cactus")
+      {
+        Console.WriteLine("You Win! Pretty fly for a cacti.");
+        Console.WriteLine("*insert ASCII cactus here*");
+      }
+      else if (TotalCareStatus >= 5 && Species == "flower")
+      {
+        Console.WriteLine("You Win! Your flower is unbeleafable.");
+        Console.WriteLine("*insert ASCII flower here*");
+      }
+      else 
+      { 
+        DetermineNextStep();
+      }
+    }
   }
-
-  
-  private static Dictionary<int, func<string, int>> _gameActions = new Dictionary<int, func<string, int>>() {1, }
 }
-
 
 
 
